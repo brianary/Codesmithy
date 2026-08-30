c4de92a042ac0c7be44dc6410bc42f0ed5ffe1a6
Brian Lalonde <brianary@gmail.com>
2026-08-30 11:59:17 -0700

    ✨ Start adding Set-GitHubStatus

---
+++ src/public/Set-GitHubStatus.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.SYNOPSIS
+Updates your profile status on GitHub.
+#>
+
+[CmdletBinding()] Param(
+[Parameter(Position=0,Mandatory=$true)][string] $Emoji,
+[Parameter(Position=1,Mandatory=$true)][string] $Status
+)
+gh api graphql --raw-field "emoji=:$($Emoji.Trim(':')):" --raw-field "status=$Status" --raw-field @'
+query=mutation UpdateStatus($emoji: String!, $status: String!) {
+	changeUserStatus(input:{emoji: $emoji, message: $status}) {
+	clientMutationId
+	status {
+			emoji
+			message
+			updatedAt
+			user {
+				login
+			}
+		}
+	}
+}
+'@
